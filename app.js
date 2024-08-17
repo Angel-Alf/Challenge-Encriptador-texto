00a23bce455f8c1f88df51c1e7b51160abbd6780
Angel-Alf <angel-thomas@hotmail.com>
2024-08-16 23:17:37 -0500

    se realiza adaptacion de css para computador de mesa

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+
+
 codigo_encritar = {
-   a:"t",b:"z",g:"m",
+   a:"t",b:"z",c:"9",
    d: "w",e: "p",f: "9",
    g: "u",h: "q",i: "r",
    j: "n",k: "s",l: "x",
@@ -12,8 +14,8 @@ codigo_encritar = {
 
 
 codigo_desencritar = {
-    t:"a",z:"b",m:"g",
-    w: "d",p: "e",9: "f",
+    t:"a",z:"b",9:"c",
+    w: "d",p: "e",g: "f",
     u: "g",q: "h",r: "i",
     n: "j",s: "k",x: "l",
     a: "m",4: "n",5: "o",
@@ -22,12 +24,6 @@ codigo_desencritar = {
     h: "v",3: "w",j: "w",
     f: "y",6: "z",2: " "
  }
-
-
-
- 
-
-
 
 
 
@@ -44,6 +40,8 @@ function encriptar (texto)
 
         imprimir(n_dato.join(""))
 
+      
+
 
     }
 
@@ -58,22 +56,27 @@ function desencriptar (texto)
 
        imprimir(n_dato.join(""))
 
+
+
         
     }
 
 
     function imprimir (texto){
 
+
         zona_printer = document.querySelector(".result_text_printer"); 
         zona_printer.innerHTML = texto;  
 
-        boton = document.querySelector(".hola")
-        boton_copiar = document.createElement("input")
-        boton_copiar.setAttribute("class", "copiar")
-        boton_copiar.setAttribute("value", "Copiar")
-        boton.appendChild(boton_copiar);
         
-    
+        copiar = zona_printer
+
+        // boton = document.querySelector(".hola")
+        // boton_copiar = document.createElement("input")
+        // boton_copiar.setAttribute("class", "copiar")
+        // boton_copiar.setAttribute("value", "Copiar")
+        // boton.appendChild(boton_copiar);
+        // document.querySelector(".input_text_zone_text").value = "";
 
     }
 
@@ -85,30 +88,53 @@ function saludo (type){
 
     zona_texto_textarea = document.querySelector(".input_text_zone_text")
     n_texto= zona_texto_textarea.value.toLowerCase()
-
+    const container = document.querySelector(".hola")
 
     if (zona_texto_textarea.value == "") {
-
-        img_espera = document.querySelector(".hola")
-        nodoImg = document.createElement("img")
-        nodoImg.setAttribute("src", "1.png")
-        img_espera.appendChild(nodoImg);
         
-        node = document.querySelector(".result_text_printer");
-        node.parentNode.removeChild(node);
+        container.classList.remove('step-1')
+        container.classList.remove('step-result')
 
-        
+        container.classList.add('step-fail')
+        // img_espera = document.querySelector(".hola")
+        // nodoImg = document.createElement("img")
+        // nodoImg.setAttribute("src", "1.png")
+        // img_espera.appendChild(nodoImg);
+        // node = document.querySelector(".result_text_printer");
+        // node.parentNode.removeChild(node);
+
+        // document.querySelector(".input_text_zone_text").value = "";
 
         return;
+
     }
     
     if (type == "encriptar"){
         encriptar(n_texto);
+        container.classList.remove('step-1')
+        container.classList.remove('step-fail')
+        container.classList.add('step-result')
+
     }else if (type == "desencriptar")
     {
         desencriptar(n_texto);
+        container.classList.remove('step-1')
+        container.classList.remove('step-fail')
+        container.classList.add('step-result')
     }
 
 }
 
 
+
+const copyContent = async () => {
+
+    let text = document.querySelector('.result_text_printer').innerHTML;
+
+  try {
+    await navigator.clipboard.writeText(text);
+    console.log(text);
+  } catch (err) {
+    console.error('Failed to copy: ', err);
+  }
+}
